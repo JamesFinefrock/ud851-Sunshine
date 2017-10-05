8c05cd2b4fe15ee36be31d34a37d4b12cf08ca55
James Finefrock <finefrockjames@gmail.com>
2017-10-05 10:34:08 -0400

    Refactored S01.02 for more dummy data

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -22,7 +22,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     // COMPLETED (1) Create a field to store the weather display TextView
-    TextView weatherDisplay;
+    TextView weatherDisplayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +30,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
-        weatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
+        weatherDisplayTextView = (TextView) findViewById(R.id.tv_weather_data);
 
         // COMPLETED (3) Create an array of Strings that contain fake weather data
-        String[] weatherData = {"Sunny", "Cloudy"};
+        String[] weatherData = {
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+                "Friday - Thunderstorms - 21°C / 9°C",
+                "Saturday - Thunderstorms - 16°C / 7°C",
+                "Sunday - Rainy - 16°C / 8°C",
+                "Monday - Partly Cloudy - 15°C / 10°C",
+                "Tue, May 24 - Meatballs - 16°C / 18°C",
+                "Wed, May 25 - Cloudy - 19°C / 15°C",
+                "Thu, May 26 - Stormy - 30°C / 11°C",
+                "Fri, May 27 - Hurricane - 21°C / 9°C",
+                "Sat, May 28 - Meteors - 16°C / 7°C",
+                "Sun, May 29 - Apocalypse - 16°C / 8°C",
+                "Mon, May 30 - Post Apocalypse - 15°C / 10°C"
+        };
 
         // COMPLETED (4) Append each String from the fake weather data array to the TextView
         for(String data : weatherData) {
-            weatherDisplay.append(data + "\n\n\n");
+            weatherDisplayTextView.append(data + "\n\n\n");
         }
     }
 }
